b6edd5c7862ebb7ca90a01c294cadbc0fafa9143
Pranay <1032241377@tcetmumbai.in>
2026-09-08 09:58:49 +0530

    Recorrecting intentional failure

--- test_model.py
+++ test_model.py
@@ -1,7 +1,7 @@
 import os
 import joblib
 def test_model_artifact_exists():
- assert os.path.exists("wrong_name.pkl"), "Error: model.pkl artifact was not created."
+ assert os.path.exists("model.pkl"), "Error: model.pkl artifact was not created."
 def test_model_inference():
  clf = joblib.load("model.pkl")
  sample_input = [[5.1, 3.5, 1.4, 0.2]]
